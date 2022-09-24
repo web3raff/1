1064fbce82183dff1725916bfe0e65950677c60c
Raff ahzani <114277610+web3raff@users.noreply.github.com>
2022-09-24 17:27:46 +0700

    Create App 1.jsx

---
+++ App 1.jsx
@@ -0,0 +1,26 @@
+
+import { Layout } from "antd";
+import "antd/dist/antd.less";
+import "./style.less";
+
+const styles = {
+ 
+};
+const App = ({ "" }) => {
+  
+
+  return (
+    <Layout
+      style={{
+        height: "100vh",
+        overflow: "auto",
+        background: "linear-gradient(#172430, #001529)",
+      }}
+    >
+     
+    </Layout>
+    
+  );
+};
+
+export default App;
